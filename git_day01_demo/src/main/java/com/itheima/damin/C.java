e0e68f877216e03c5e9b4b3a94058990bd206c93
linyao <2507995429@qq.com>
2019-09-03 21:13:40 +0800

    添加了C 分线

---
+++ git_day01_demo/src/main/java/com/itheima/damin/C.java
@@ -0,0 +1,17 @@
+package com.itheima.damin;/**
+ * @Auther: ${LY}
+ * @Date: 2019.9.3 21:09
+ * @Description:
+ */
+
+/**
+ * @program: git_day01_demo
+ *
+ * @description:
+ *
+ * @author: Mr.Wang
+ *
+ * @create: 2019-09-03 21:09
+ **/
+public class C {
+}
--- git_day01_demo/src/main/java/com/itheima/damin/User.java
+++ git_day01_demo/src/main/java/com/itheima/damin/User.java
@@ -3,11 +3,11 @@ package com.itheima.damin;
 public class User {
     private  String id;
 
-    private  Integer Id;
+    private  String name;
 
     private  String education;
 
-    private  Integer  age ;
+    private String fenian;
 
-    private  String user;
+
 }
